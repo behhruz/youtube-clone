8c981ed6283488d129adc0663a55b5d83bc62b1c
behhruz <bsatimbayev05@gmail.com>
2023-01-15 22:26:03 +0500

    Start

---
+++ src/context/index.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+import React from "react";
+export const VidioContext = createContext([]);
+const HooksContext = () => {
+  return {};
+};
+export default HooksContext;
---
+++ src/utilities/backend1.js
@@ -0,0 +1,91 @@
+import Img from "../assets/image/1.png";
+let arr = [
+  {
+    id: 1,
+    vidio: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/bMknfKXIFA8"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+    rol: "vidio",
+    subscribed: true,
+    img: Img,
+    text: "React lesson",
+    name: "James Couse",
+    watch: "15k view",
+    data: "1 hours ago",
+    type: "programming",
+  },
+  {
+    id: 2,
+    vidio: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/vGQZB_RiOIY"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+    rol: "vidio",
+    subscribed: true,
+    img: Img,
+    text: "Music by Benom",
+    name: "Benom_offical",
+    watch: "16m view",
+    data: "3 years ago",
+    type: "Music",
+  },
+  {
+    id: 3,
+    vidio: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/fs4co7-8UEE"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+    rol: "vidio",
+    subscribed: true,
+    img: Img,
+    text: "Littel family interview",
+    name: "Jahongir Latipov",
+    watch: "7m view",
+    data: "3 days ago",
+    type: "interview",
+  },
+  {
+    id: 4,
+    vidio: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/RI_oca0GFQc"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+    rol: "vidio",
+    subscribed: true,
+    img: Img,
+    text: "Ramazan Temirov vc Palonchi",
+    name: "Bugun Sport",
+    watch: "20m view",
+    data: "3 moths ago",
+    type: "sport",
+  },
+];
